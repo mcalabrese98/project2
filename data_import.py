3278c6755b17946520b6c129b907df5da1d7e4e2
Stefan Holodnick <stefan.holodnick@suitecentric.com>
2024-10-24 22:15:57 -0400

    Cleaned up result data

--- data_import.py
+++ data_import.py
@@ -18,7 +18,7 @@ def get_tmdb_movie_list(page_num=None, max_page_num=1):
 
   for i in range(page_num, max_page_num):
     if (i % 50 == 0):
-        time.sleep(20)
+        time.sleep(1)
 
     movie_list_url = f"https://api.themoviedb.org/3/discover/movie?&api_key={tmdb_api_key}&page={i}&release_date.lte={current_date}"
 
@@ -36,7 +36,7 @@ def get_tmdb_movie_details(movie_list):
   index = 0
   for movie in movie_list:
     if (index % 50 == 0):
-        time.sleep(20)
+        time.sleep(1)
 
     movie_id = movie['id']
     movie_details = {}
@@ -56,28 +56,42 @@ def get_tmdb_movie_details(movie_list):
 def get_tmdb_movie_credits(movie_list):
   movie_credits_list = []
 
+  index = 0
   for movie in movie_list:
+    if (index % 50 == 0):
+        time.sleep(1)
+
     movie_id = movie['id']
 
     movie_credits_url = f"https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key={tmdb_api_key}"
 
     movie_credits_list.append(requests.get(movie_credits_url).json())
 
+    index += 1
+
   return movie_credits_list
 
 
 if __name__ == "__main__":
   movie_list = get_tmdb_movie_list(max_page_num=50)
-  movie_detail_list = get_tmdb_movie_details(movie_list)
+  # movie_detail_list = get_tmdb_movie_details(movie_list)
 
-  movie_detail_list_flattened = [flatten(movie) for movie in movie_detail_list]
+  # movie_detail_list_flattened = [flatten(movie) for movie in movie_detail_list]
 
-  movie_df = pd.DataFrame(movie_detail_list_flattened)
+  # movie_df = pd.DataFrame(movie_detail_list_flattened)
 
-  columns_to_remove = ['belongs_to_collection_id', 'belongs_to_collection_name', 'belongs_to_collection_poster_path', 'belongs_to_collection_backdrop_path', 'genres_0_id', 'genres_1_id', 'genres_2_id', 'genres_3_id', 'genres_4_id', 'spoken_languages_0_name']
+  # columns_to_remove = ['belongs_to_collection_id', 'belongs_to_collection_name', 'belongs_to_collection_poster_path', 'belongs_to_collection_backdrop_path', 'genres_0_id', 'genres_1_id', 'genres_2_id', 'genres_3_id', 'genres_4_id', 'spoken_languages_0_name']
 
-  movie_df.drop(columns=columns_to_remove, inplace=True)
+  # movie_df.drop(columns=columns_to_remove, inplace=True)
 
-  movie_df = movie_df[sorted(movie_df.columns)]
+  # movie_df = movie_df[sorted(movie_df.columns)]
 
-  movie_df.to_csv("movie_data.csv")
+  # movie_df.to_csv("movie_data.csv")
+
+  movie_credits_list = get_tmdb_movie_credits(movie_list)
+
+  movie_credits_list_flattened = [flatten(movie) for movie in movie_credits_list]
+
+  movie_credits_df = pd.DataFrame(movie_credits_list_flattened)
+
+  movie_credits_df.to_csv("movie_credits_data.csv")
